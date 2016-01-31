9f997b850f475e4a2eec57908dd74230b2fc789a
Matthew Dean <deanmatthew16@gmail.com>
2016-01-31 13:05:28 -0500

    Show explanation for FileNotFoundException

--- lib/src/test/kotlin/net/dean/easyarchive/library/test/TestUtils.kt
+++ lib/src/test/kotlin/net/dean/easyarchive/library/test/TestUtils.kt
@@ -5,9 +5,15 @@ import net.dean.easyarchive.library.test.TestUtils.md5
 import java.io.File
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
+import java.io.FileNotFoundException
 
 public fun resource(path: String): File = File(File(Inflater::class.java.getResource("/").toURI()).parentFile.parentFile, "resources/test/$path")
-public fun sample(extension: String): File = resource("sample.$extension")
+public fun sample(extension: String): File {
+    val f = resource("sample.$extension")
+    if (!f.exists())
+        throw FileNotFoundException("${f.absolutePath} does not exist. Did you run mk.sh?")
+    return f
+}
 public val tempDir: File = File(System.getProperty("java.io.tmpdir"), "EasyArchive")
 public fun tempDir(extension: String): File = File(tempDir, "out/$extension")
 public fun md5Comp(original: File, new: File) {
