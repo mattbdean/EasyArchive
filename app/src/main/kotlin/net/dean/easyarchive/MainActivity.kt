268028189f80951d628e249f7d2cc9e052191903
Matthew Dean <deanmatthew16@gmail.com>
2016-02-01 21:58:32 -0500

    Inflate in the background

--- app/src/main/kotlin/net/dean/easyarchive/MainActivity.kt
+++ app/src/main/kotlin/net/dean/easyarchive/MainActivity.kt
@@ -1,5 +1,6 @@
 package net.dean.easyarchive
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -7,6 +8,8 @@ import com.pawegio.kandroid.d
 import com.pawegio.kandroid.find
 import net.dean.easyarchive.library.ArchiveEvent
 import net.dean.easyarchive.library.ArchiveEventHandler
+import net.dean.easyarchive.library.InflaterAggregation
+import java.io.File
 
 public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,13 +18,27 @@ public class MainActivity : AppCompatActivity() {
     }
 
     fun unarchive(view: View) {
-        EasyArchive.inflaters.eventHandler = object: ArchiveEventHandler {
+        InflateTask().execute(find<FileInputView>(R.id.input_file).file(),
+                find<FileInputView>(R.id.input_directory).file())
+    }
+}
+
+public class InflateTask : AsyncTask<File, ArchiveEvent, Unit>() {
+    override fun doInBackground(vararg params: File?) {
+        val inflaters = InflaterAggregation()
+        inflaters.eventHandler = object: ArchiveEventHandler {
             override fun handle(e: ArchiveEvent) {
-                d(e.toString())
+                onProgressUpdate(e)
             }
         }
-        EasyArchive.inflaters.inflate(
-                find<FileInputView>(R.id.input_file).file(),
-                find<FileInputView>(R.id.input_directory).file())
+        if (params.size != 2)
+            throw IllegalArgumentException("Something other than a file and a directory was given")
+        val file = params[0]!!
+        val directory = params[1]!!
+        inflaters.inflate(file, directory)
+    }
+
+    override fun onProgressUpdate(vararg values: ArchiveEvent?) {
+        d(values[0].toString())
     }
 }
