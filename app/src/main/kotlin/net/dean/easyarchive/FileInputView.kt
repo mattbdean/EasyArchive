f2ccf5c7eff9bb7baa3a8d1ac0f2172a648e8f97
Matthew Dean <deanmatthew16@gmail.com>
2016-02-01 19:17:59 -0500

    Add button for unarchiving

--- app/src/main/kotlin/net/dean/easyarchive/FileInputView.kt
+++ app/src/main/kotlin/net/dean/easyarchive/FileInputView.kt
@@ -16,13 +16,17 @@ public class FileInputView : RelativeLayout {
     private var validationStatus: ValidationStatus? = null
         set(value) {
             if (value == null) throw NullPointerException("validationStatus cannot be null")
-            find<TextView>(R.id.status).setText(statusFor(value))
-            find<ImageView>(R.id.status_icon).setImageResource(iconFor(value.severity))
+            status.setText(statusFor(value))
+            statusIcon.setImageResource(iconFor(value.severity))
             field = value
         }
     private var inputMode: InputMode by TardyNotNullVal()
     private val hint: Int
         get() = if (inputMode == InputMode.FILE) R.string.hint_input_file else R.string.hint_input_directory
+
+    private val filename: EditText by lazy { find<EditText>(R.id.filename) }
+    private val status: TextView by lazy { find<TextView>(R.id.status) }
+    private val statusIcon: ImageView by lazy { find<ImageView>(R.id.status_icon) }
 
     constructor(context: Context) : super(context) {
         inputMode = InputMode.FILE
@@ -34,14 +38,15 @@ public class FileInputView : RelativeLayout {
         initialize(context, attrs)
     }
 
-    public fun initialize(context: Context, attrs: AttributeSet) {
+    private fun initialize(context: Context, attrs: AttributeSet) {
         val array = context.theme.obtainStyledAttributes(attrs, R.styleable.FileInputView, 0, 0)
         inputMode = InputMode.values()[array.getInteger(R.styleable.FileInputView_inputMode, 0)]
 
         inflate(context, R.layout.view_file_input, this)
 
+        filename.setHint(hint)
         if (!isInEditMode) {
-            find<EditText>(R.id.filename).textWatcher {
+            filename.textWatcher {
                 afterTextChanged { text ->
                     val f = File(text.toString())
                     validationStatus = if (inputMode == InputMode.FILE)
@@ -50,16 +55,18 @@ public class FileInputView : RelativeLayout {
                         EasyArchive.inflaters.validateDestination(f)
                 }
             }
-            find<EditText>(R.id.filename).setHint(hint)
         } else {
-            find<EditText>(R.id.filename).setText(hint)
             find<TextView>(R.id.status).setText(R.string.vs_ready)
             find<ImageView>(R.id.status_icon).setImageResource(R.drawable.ic_good)
         }
     }
 
+    public fun file() = File(filename.text.toString())
+
     private fun statusFor(s: ValidationStatus?): Int =
-            if (s == null) 0 else context.resources.getIdentifier("vs_${s.name.toLowerCase()}", "string", "net.dean.easyarchive")
+            if (s == null) 0 else
+                context.resources.getIdentifier("vs_${s.name.toLowerCase()}", "string", "net.dean.easyarchive")
+
     private fun iconFor(s: Severity): Int =
             if (s == Severity.SEVERE) R.drawable.ic_bad else R.drawable.ic_good
 }
--- app/src/main/kotlin/net/dean/easyarchive/MainActivity.kt
+++ app/src/main/kotlin/net/dean/easyarchive/MainActivity.kt
@@ -2,10 +2,26 @@ package net.dean.easyarchive
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.pawegio.kandroid.d
+import com.pawegio.kandroid.find
+import net.dean.easyarchive.library.ArchiveEvent
+import net.dean.easyarchive.library.ArchiveEventHandler
 
 public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun unarchive(view: View) {
+        EasyArchive.inflaters.eventHandler = object: ArchiveEventHandler {
+            override fun handle(e: ArchiveEvent) {
+                d(e.toString())
+            }
+        }
+        EasyArchive.inflaters.inflate(
+                find<FileInputView>(R.id.input_file).file(),
+                find<FileInputView>(R.id.input_directory).file())
     }
 }
